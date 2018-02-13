97fb5987a9a72d06e24d65b4df80fc6b31d0c4dc
Brett Leibowitz <brettleibo@gmail.com>
2018-02-13 10:51:36 -0500

    import files

--- pictureServer.js
+++ pictureServer.js
@@ -31,6 +31,9 @@ let NodeWebcam = require( "node-webcam" );// load the webcam module
 
 let gifshot = require('gifshot')
 
+app.use('/static/simple-slideshow/simple-slideshow.css', express.static('./node_modules/simple-slideshow/src/slideshow.css'));
+app.use('/static/simple-slideshow/simple-slideshow.js', express.static('./node_modules/simple-slideshow/src/slideshow.js'));
+
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
 // use express to create the simple webapp
 app.use(express.static('public')); // find pages in public directory
